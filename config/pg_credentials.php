d65bc6afbb14332b1f25024be63fb75f1393a47c
stepupmin <33408883+stepupmin@users.noreply.github.com>
2018-03-29 23:42:37 +0700

    Create pg_credentials.php

---
+++ config/pg_credentials.php
@@ -0,0 +1,6 @@
+<?php
+$host = "host = ec2-54-243-130-33.compute-1.amazonaws.com ";
+$dbname = "dbname = d55plguu27h70h ";
+$user = "user = tdybpzwmmffehg ";
+$password = "password = 52eaa707ede4a48a7a1dfe5d9d160fc07e7a3fe2a84597f0c53fdef040bb7d85";
+?>

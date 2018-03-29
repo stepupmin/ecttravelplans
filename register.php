feb9902e8d99b8cb4c96d7e463f4e676e879277f
stepupmin <33408883+stepupmin@users.noreply.github.com>
2018-03-30 01:09:16 +0700

    Update register.php

--- register.php
+++ register.php
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$users = json_decode($content, true);
 	
 		$action = $users['action'];
-		$fbid = $users['$fbid'];
+		$fbid = $users['fbid'];
 		
 		// check action
 		if(strcmp($action, 'register') == 0){

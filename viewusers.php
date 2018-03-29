c1331a97df4013979ddc4ff123fc29681aa115b0
stepupmin <33408883+stepupmin@users.noreply.github.com>
2018-03-29 23:45:56 +0700

    Create viewusers.php

---
+++ viewusers.php
@@ -0,0 +1,25 @@
+<?php
+// Connecting, selecting database
+require("/config/connectionDB.php");
+
+// Performing SQL query
+$query = 'SELECT * FROM users';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+// Printing results in HTML
+$data = array();
+while ($row = pg_fetch_assoc($result)) {
+    array_push($data, array(
+        'id' => $row['id'],
+        'username' => $row['username'],
+        'name' => $row['name'],
+        'password' => $row['password'],
+        'email' => $row['email']
+    ));
+}
+$json = json_encode($data);
+echo $json;
+// Free resultset
+pg_free_result($result);
+// Closing connection
+pg_close($dbconn);
+?>

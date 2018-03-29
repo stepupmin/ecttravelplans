f1a40dbe5c61b14a8ee9df5db4361fa08c779fe3
stepupmin <33408883+stepupmin@users.noreply.github.com>
2018-03-29 23:35:04 +0700

    Update request.php

--- request.php
+++ request.php
@@ -1,14 +1,6 @@
 <?php
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	$name= $_POST['name'];
-	$email= $_POST['email'];
-	
-	$arrPostData = array();
-	$arrPostData['username'] = $username;
-	$arrPostData['password'] = $password;
-	$arrPostData['name'] = $name;
-	$arrPostData['email'] = $email;
+	$username = $_GET['username'];
+
 
 	echo $username;
 ?>
